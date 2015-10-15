2e9b6136cc247b7586e5c2507248d0630b9b312a
Martin <ogradymartin7@gmail.com>
2015-10-15 16:50:16 +0200

    Abstracting the auth api class.
    Making the authentication class not dependant on the GUI

--- dev/app/client/controllers/initAuth.js
+++ dev/app/client/controllers/initAuth.js
@@ -2,63 +2,50 @@
 
 var $ = require('jquery');
 
+var authorized = false;
+
 var Auth = function(){
   var clientId;
   var OAUTH2_SCOPES;
+  /** I need to set the authorized but would love to have it hidden somehwer**/
+  this.getAuthorized = function(){
+    return authorized;
+  };
+
   this.getClientID = function(){
     return clientId = '331947551227-0u2kbgm1kdt0tm4vgefhsoecrrodid2g.apps.googleusercontent.com';
-  }
+  };
+
   this.getOAUTH2_SCOPES = function(){
     return OAUTH2_SCOPES = 'https://www.googleapis.com/auth/youtube';
-  }
+  };
 };
 
 Auth.prototype.googleApiClientReady = function(){
-  var self = this;
-  gapi.auth.init(function() {
-    window.setTimeout(self.checkAuth.bind(self), 1);
+  return new Promise((resolve) => {
+    gapi.auth.init(() => {
+      this.checkAuth().then(function(){
+        resolve();
+      });
+    });
   });
 };
 
 Auth.prototype.checkAuth = function(){
-  var self = this;
+  return new Promise((resolve) => {
     gapi.auth.authorize({
-	  client_id: self.getClientID(),
-	  scope: self.getOAUTH2_SCOPES(),
-	  immediate: true },
-	  function(token){
-	    if (token.access_token) {
-          console.log('Authorized!');
-          $('#loading').hide();
-          $('#postAuth').show();
-          console.log("here?");
-          setTimeout(function(){
-          $('#wrap').addClass('test');
-        });
-          self.loadAPIClientInterfaces();
-        }else{
-          $('.loading').hide();
-          $('#preAuth').show();
-          console.log('not Authorized');
-        }
-	});
-};
-
-Auth.prototype.handleAuthResult = function(authResult) {
-  console.log('in here?');
-  console.log(authResult.error);
-  if (authResult && !authResult.error) {
-    console.log("ehhhhh");
-    // Authorization was successful. Hide authorization prompts and show
-	// content that should be visible after authorization succeeds.
-	$('#pre-auth').hide();
-  $('#post-auth').show();
-	this.loadAPIClientInterfaces();
-  } else if (authResult && authResult.error) {
-    // TODO: Show error
-	console.error('Unable to sign in:', authResult.error);
-  }
-	console.log('hmmm');
+	    client_id: this.getClientID(),
+	    scope: this.getOAUTH2_SCOPES(),
+	    immediate: true
+    },
+	   (token) => {
+	     if (token.access_token) {
+         this.loadAPIClientInterfaces();
+         authorized = true;
+       }
+       resolve();    
+	  });
+  });
 };
 
 Auth.prototype.loadAPIClientInterfaces = function() {
